c8f210387a2b7c554a214382c0740eef3501990b
sigespweb22 <alan.rezendeeee@hotmail.com>
2024-02-25 12:52:09 -0300

    feat: implementações diversas

---
+++ src/App.svelte
@@ -0,0 +1,31 @@
+<script lang="ts">
+    import Botao from "./components/Botao.svelte";
+</script>
+
+<main>
+	<Botao texto="5" />
+	<Botao texto="6" />
+	<Botao texto="7" />
+</main>
+
+<style>
+	main {
+		text-align: center;
+		padding: 1em;
+		max-width: 240px;
+		margin: 0 auto;
+	}
+
+	h1 {
+		color: #ff3e00;
+		text-transform: uppercase;
+		font-size: 4em;
+		font-weight: 100;
+	}
+
+	@media (min-width: 640px) {
+		main {
+			max-width: none;
+		}
+	}
+</style>
---
+++ src/components/Botao.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+    export let texto:string = '';
+</script>
+
+<button class="button">
+    {texto}
+</button>
+
+<style>
+    .button {
+
+    }
+</style>
